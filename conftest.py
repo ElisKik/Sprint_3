586977906605b46a20d0e821ff527ed734b1890d
Elis Kik <kik_elis@mail.ru>
2023-04-08 12:27:45 +0300

    Remove usage of environment variables

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ Script for definitions of fixtures.
 
 import pytest
 
-from os import environ
 from typing import Iterable
 
 from selenium.webdriver import Chrome as WebDriver
@@ -20,19 +19,13 @@ def webdriver() -> Iterable[WebDriver]:
     with maximized window and headless mode
     by default.
     """
-
-    is_headless = bool(int(environ.get('SELENIUM_HEADLESS', '1')))
-    is_maximized = bool(int(environ.get('SELENIUM_MAXIMIZED', '1')))
-
     options = Options()
 
-    if is_headless:
-        options.add_argument('--headless')
+    options.add_argument('--headless')
 
     webdriver = WebDriver(options=options)
 
-    if is_maximized:
-        webdriver.maximize_window()
+    webdriver.maximize_window()
 
     yield webdriver
 
